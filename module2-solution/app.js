f9dc594f1f5656012e85bb28401126d4376a2185
chocolove2513 <lizer132@yahoo.com.mx>
2016-11-22 20:16:11 -0600

    solution 2

---
+++ module2-solution/app.js
@@ -0,0 +1,120 @@
+(function () {
+  'use strict';
+
+  angular.module('ShoppingListCheckOff',[])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+// ToBuy Controller
+  ToBuyController.$inject=['ShoppingListCheckOffService'];
+
+  function ToBuyController(ShoppingListCheckOffService) {
+    var showToBuyItems = this;
+    //Show all items
+    showToBuyItems.items = ShoppingListCheckOffService.getToBuyItems();
+
+
+    showToBuyItems.bought = function ($index) {
+      try {
+        ShoppingListCheckOffService.addBouhtItems($index);
+        ShoppingListCheckOffService.removeBuyItems($index);
+      } catch (error) {
+        showToBuyItems.error = error.message;
+      }
+    }
+  }
+
+  // ToBought Controller
+   AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+
+   function AlreadyBoughtController(ShoppingListCheckOffService) {
+     var alreadyBoughtItems = this;
+     try {
+       alreadyBoughtItems.items = ShoppingListCheckOffService.getBoughtItems();
+     } catch (error) {
+       alreadyBoughtItems.error = error.message;
+       console.log("error asdf");
+     }
+
+
+
+   }
+
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+    var boughtItems = [];
+    var toBuyItems = [
+      {
+        name : "cookies",
+        quantity : "10"
+      },
+      {
+        name : "bottle of juice",
+        quantity : "5"
+      },
+      {
+        name : "soda",
+        quantity : "3"
+      },
+      {
+        name : "gallons of milk",
+        quantity : "4"
+      },
+      {
+        name : "loaves of bread",
+        quantity : "10"
+      },
+      {
+        name : "cans of soup",
+        quantity : "12"
+      },
+      {
+        name : "pounds of meat",
+        quantity : "3"
+      },
+      {
+        name : "dozen eggs",
+        quantity : "14"
+      },
+      {
+        name : "bottles of jam",
+        quantity : "5"
+      },
+      {
+        name : "bottles of mayonnaise",
+        quantity : "6"
+      }
+    ];
+
+    service.getToBuyItems = function () {
+      return toBuyItems;
+    };
+
+    service.getBoughtItems = function () {
+      if (boughtItems.length < 0) {
+          throw new Error("Nothing bought yet");
+          console.log("here");
+      }else {
+        return boughtItems;
+      }
+
+    };
+
+    service.addBouhtItems = function ($index) {
+
+        boughtItems.push(toBuyItems[$index]);
+
+    };
+
+    service.removeBuyItems = function ($index) {
+        toBuyItems.splice(toBuyItems[$index],1);
+        console.log($index);
+        if (boughtItems.length === 10) {
+          console.log("here");
+          throw new Error ("Everything is bought");
+        }
+    }
+  }
+})();
